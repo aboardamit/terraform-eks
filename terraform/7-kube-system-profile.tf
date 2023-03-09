7917ec701fe3d7d2c357a55e9e4082a77cf70b26
Amit Kumar <Amit.Kumar85@ie.ey.com>
2023-03-09 11:57:42 +0000

    Patch CoreDNS

--- terraform/7-kube-system-profile.tf
+++ terraform/7-kube-system-profile.tf
@@ -21,8 +21,8 @@ resource "aws_iam_role_policy_attachment" "eks-fargate-profile" {
 }
 
 resource "aws_eks_fargate_profile" "kube-system" {
-  cluster_name = aws_eks_cluster.cluster.name
-  fargate_profile_name = "kube-system"
+  cluster_name           = aws_eks_cluster.cluster.name
+  fargate_profile_name   = "kube-system"
   pod_execution_role_arn = aws_iam_role.eks-fargate-profile.arn
 
   subnet_ids = [
@@ -32,5 +32,38 @@ resource "aws_eks_fargate_profile" "kube-system" {
 
   selector {
     namespace = "kube-system"
+  }
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = aws_eks_cluster.cluster.id
+}
+
+resource "null_resource" "k8s_patcher" {
+  depends_on = [aws_eks_fargate_profile.kube-system]
+
+  triggers = {
+    endpoint = aws_eks_cluster.cluster.endpoint
+    ca_crt   = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
+    token    = data.aws_eks_cluster_auth.eks.token
+  }
+
+  provisioner "local-exec" {
+    command = <<EOH
+        cat >/tmp/ca.crt <<EOF
+${base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)}
+EOF
+kubectl \
+  --server="${aws_eks_cluster.cluster.endpoint}" \
+  --certificate_authority=/tmp/ca.crt \
+  --token="${data.aws_eks_cluster_auth.eks.token}" \
+  patch deployment coredns \
+  -n kube-system --type json \
+  -p='[{"op": "remove", "path": "/spec/template/metadata/annotations/eks.amazonaws.com~1compute-type"}]'
+EOH
+  }
+
+  lifecycle {
+    ignore_changes = [triggers]
   }
 }
